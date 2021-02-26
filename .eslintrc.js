934b4ed102e29515be50ad5362d42bef973ef80f
yyz <yuanyuzhangyyzz@163.com>
2021-02-26 23:43:04 +0800

    add prettier and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    jest: true,
+    browser: true,
+    es2021: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react'],
+  parser: 'babel-eslint',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        printWidth: 80,
+        tabWidth: 2,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
