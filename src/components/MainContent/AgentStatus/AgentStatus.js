6df4dd870ba145c172e550dfc0c749c07c00f305
yyz <yuanyuzhangyyzz@163.com>
2021-02-27 15:41:09 +0800

    adjust the size and color of StatusCard component

--- src/components/MainContent/AgentStatus/AgentStatus.js
+++ src/components/MainContent/AgentStatus/AgentStatus.js
@@ -5,8 +5,18 @@ import './styles.scss';
 export function AgentStatus() {
   return (
     <div className="agentStatus">
-      <StatusCard status="Building" count={3} iconName="icon-cog"></StatusCard>
-      <StatusCard status="Idle" count={5} iconName="icon-coffee"></StatusCard>
+      <StatusCard
+        status="Building"
+        count={3}
+        iconName="icon-cog"
+        cardColor={'yellow'}
+      ></StatusCard>
+      <StatusCard
+        status="Idle"
+        count={5}
+        iconName="icon-coffee"
+        cardColor={'green'}
+      ></StatusCard>
     </div>
   );
 }
--- src/components/MainContent/AgentStatus/StatusCard/StatusCard.js
+++ src/components/MainContent/AgentStatus/StatusCard/StatusCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-export function StatusCard({ status, count, iconName }) {
+export function StatusCard({ status, count, iconName, cardColor }) {
   return (
-    <div className="statusCard">
+    <div className={`statusCard ${cardColor}`}>
       <div className="status">{status}</div>
       <div className={`icon ${iconName}`}></div>
       <div className="countNumber">{count}</div>
@@ -14,6 +14,7 @@ export function StatusCard({ status, count, iconName }) {
 
 StatusCard.propTypes = {
   iconName: PropTypes.string.isRequired,
+  cardColor: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
 };
