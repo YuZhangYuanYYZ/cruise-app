8e4aec3bf05982ad77a5fee0fb056ea7a2894821
yyz <yuanyuzhangyyzz@163.com>
2021-02-27 14:13:51 +0800

    add empty right content

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function MainContent() {
+  return (
+    <div>
+      <div className="agentStatusContainer">Status</div>
+      <div className="agentResourceContainer"></div>
+    </div>
+  );
+}
