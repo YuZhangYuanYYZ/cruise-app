6bad7561e9431aaca613f0142d94e0fb8e87a11f
yyz <yuanyuzhangyyzz@163.com>
2021-02-27 16:09:14 +0800

    store colors and fontsize in one center file to reuse

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { MainContent } from './components/MainContent';
 import { SideBar } from './components/SideBar';
 import { TopHeader } from './components/TopHeader';
