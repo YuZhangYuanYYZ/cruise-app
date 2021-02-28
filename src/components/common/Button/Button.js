ed94b71e1e263763d042f65639f72c8956f4c8be
yyz <yuanyuzhangyyzz@163.com>
2021-02-28 23:35:43 +0800

    change button component

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -2,5 +2,14 @@ import React from 'react';
 import './styles.scss';
 
 export function Button(props) {
-  return <button className="customButton">{props.text}</button>;
+  const renderIcon = (iconName) => {
+    return <span className={`icon ${iconName}`}></span>;
+  };
+
+  return (
+    <button className="customButton primary">
+      {props.iconName && renderIcon(props.iconName)}
+      {props.children}
+    </button>
+  );
 }
