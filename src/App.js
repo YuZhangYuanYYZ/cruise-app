584ecd58547fd1c515a18471f673c14eb77d4404
yyz <yuanyuzhangyyzz@163.com>
2021-02-28 11:00:12 +0800

    pass store in App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.scss';
 import { MainContent } from './components/MainContent';
 import { SideBar } from './components/SideBar';
 import { TopHeader } from './components/TopHeader';
-
+import { Provider } from 'react-redux';
+import store from './store';
 function App() {
   return (
-    <div className="App">
-      <TopHeader></TopHeader>
-      <div className="content">
-        <SideBar></SideBar>
-        <MainContent></MainContent>
+    <Provider store={store}>
+      <div className="App">
+        <TopHeader></TopHeader>
+        <div className="content">
+          <SideBar></SideBar>
+          <MainContent></MainContent>
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
