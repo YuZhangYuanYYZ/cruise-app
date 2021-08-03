93ab01317bfd60b398b74fc3bae0ee11f2495f77
yyz <yuanyuzhangyyzz@163.com>
2021-08-03 22:11:38 +0800

    add the pagination files to the project

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { AgentFilter } from './AgentFilter/AgentFilter';
 import { AgentList } from './AgentList';
 import { AgentStatus } from './AgentStatus';
 import { NaveSearch } from './NaveSearch';
 import { AddAgent } from './AddAgent';
+import { Pagination } from '../common/Pagination/Pagination';
 import './styles.scss';
 export function MainContent() {
+  let agentLists = useSelector((state) => {
+    return state.agents.items;
+  });
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="mainContent">
       <AgentStatus />
@@ -17,6 +23,13 @@ export function MainContent() {
         </div>
         <AgentList></AgentList>
       </div>
+      <Pagination
+        className="paginations"
+        currentPage={currentPage}
+        totalCount={agentLists.length}
+        pageSize={2}
+        onPageChange={(page) => setCurrentPage(page)}
+      ></Pagination>
     </div>
   );
 }
